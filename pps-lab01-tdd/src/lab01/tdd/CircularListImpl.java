1c6afef5c096c31e1f6b6a33dfec6e53a71a6aff
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-02-28 16:42:02 +0100

    Completed step 1 exercise 2

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,62 @@
+package lab01.tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+
+    private int actual = -1;
+    private final List<Integer> list = new ArrayList<>();
+
+    @Override
+    public void add(int element) {
+        list.add(element);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        if (isEmpty()) {
+            return Optional.empty();
+        } else {
+            actual = (actual + 1) % size();
+            return Optional.of(list.get(actual));
+        }
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        if (isEmpty()) {
+            return Optional.empty();
+        } else {
+            int tmp = list.get((size() + actual) % size());
+            actual = (size() + actual - 1) % size();
+            return Optional.of(tmp);
+        }
+    }
+
+    @Override
+    public void reset() {
+        if (!isEmpty()) {
+            int tmp = list.get((actual + 1) % size());
+            list.remove((actual + 1) % size());
+            list.add(0, tmp);
+            actual = -1;
+        }
+    }
+
+    @Override
+    public Optional<Integer> next(SelectStrategy strategy) {
+        return Optional.empty();
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,136 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList list;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach(){
+        list = new CircularListImpl();
     }
 
+    @Test
+    public void testCreatedEmpty(){
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testNextEmpty(){
+        assertEquals(Optional.empty(), list.next());
+    }
+
+    @Test
+    public void testPreviousEmpty(){
+        assertEquals(Optional.empty(), list.previous());
+    }
+
+
+    @Test
+    public void testResetEmpty(){
+        list.reset();
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testAdd(){
+        list.add(1);
+        assertFalse(list.isEmpty());
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void testNextWithOneElement(){
+        list.add(1);
+        assertEquals(Optional.of(1), list.next());
+    }
+
+    @Test
+    public void testNextLinkedWithOneElement(){
+        list.add(1);
+        assertEquals(Optional.of(1), list.next());
+        assertEquals(Optional.of(1), list.next());
+    }
+
+    @Test
+    public void testNextWithTwoElements(){
+        list.add(1);
+        list.add(2);
+        assertEquals(Optional.of(1), list.next());
+        assertEquals(Optional.of(2), list.next());
+    }
+
+    @Test
+    public void testNextLinkedWithTwoElements(){
+        list.add(1);
+        list.add(2);
+        assertEquals(Optional.of(1), list.next());
+        assertEquals(Optional.of(2), list.next());
+        assertEquals(Optional.of(1), list.next());
+        assertEquals(Optional.of(2), list.next());
+    }
+
+    @Test
+    public void testPreviousWithOneElement(){
+        list.add(1);
+        assertEquals(Optional.of(1), list.previous());
+    }
+
+    @Test
+    public void testPreviousLinkedWithOneElement(){
+        list.add(1);
+        assertEquals(Optional.of(1), list.previous());
+        assertEquals(Optional.of(1), list.previous());
+    }
+
+    @Test
+    public void testPreviousWithTwoElements(){
+        list.add(1);
+        list.add(2);
+        assertEquals(Optional.of(2), list.previous());
+        assertEquals(Optional.of(1), list.previous());
+    }
+
+    @Test
+    public void testPreviousLinkedWithTwoElements(){
+        list.add(1);
+        list.add(2);
+        assertEquals(Optional.of(2), list.previous());
+        assertEquals(Optional.of(1), list.previous());
+        assertEquals(Optional.of(2), list.previous());
+        assertEquals(Optional.of(1), list.previous());
+    }
+
+    @Test
+    public void testNextPrevious(){
+        list.add(1);
+        list.add(2);
+        assertEquals(Optional.of(2), list.previous());
+        assertEquals(Optional.of(2), list.next());
+        assertEquals(Optional.of(1), list.next());
+    }
+
+    @Test
+    public void testReset(){
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        assertEquals(Optional.of(3), list.previous());
+        assertEquals(Optional.of(2), list.previous());
+        list.reset();
+        assertEquals(Optional.of(2), list.next());
+        assertEquals(Optional.of(1), list.next());
+        assertEquals(Optional.of(3), list.next());
+    }
 }
