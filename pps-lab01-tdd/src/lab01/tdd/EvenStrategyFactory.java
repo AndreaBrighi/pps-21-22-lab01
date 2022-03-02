7e29b60f400d1718ee2a6eb6781edd5d26e68b2c
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-03-02 22:07:45 +0100

    Used class MultipleOfStrategyFactory for implementation of EvenStrategyFactory

--- pps-lab01-tdd/src/lab01/tdd/EvenStrategyFactory.java
+++ pps-lab01-tdd/src/lab01/tdd/EvenStrategyFactory.java
@@ -1,8 +1,25 @@
 package lab01.tdd;
 
-public class EvenStrategyFactory implements SelectAbstractFactory{
+/**
+ * This class create SelectStrategy that check only even elements
+ */
+public class EvenStrategyFactory extends MultipleOfStrategyFactory {
+
+    /**
+     * Constructor of the class EvenStrategyFactory
+     *
+     */
+    public EvenStrategyFactory() {
+        super(2);
+    }
+
+    /**
+     * Create a new SelectStrategy that check only even elements
+     *
+     * @return a new SelectStrategy
+     */
     @Override
     public SelectStrategy createSelectStrategy() {
-        return i -> i % 2 == 0;
+        return super.createSelectStrategy();
     }
 }
